4ed652259f63d92be0d831f7caef4aa74f3c7304
Quincey James <contact@quinceyjames.com>
2023-01-11 15:02:15 -0500

    The Config class can now parse the sourcecode heuristics in the config file

--- guarddog/configs/config.py
+++ guarddog/configs/config.py
@@ -27,6 +27,25 @@ class HeuristicConfig(ABC):
         pass
 
 
+class SourcecodeConfig(HeuristicConfig):
+    def join(self, *configs: 'SourcecodeConfig') -> 'SourcecodeConfig':
+        def get_first(keyword):
+            for config in [self, *configs]:
+                if config and config.__dict__[keyword]:
+                    return config.__dict__[keyword]
+
+        return (
+            SourcecodeConfig(
+                key=get_first("key"),
+                name=get_first("name"),
+                description=get_first("description"),
+                category=get_first("category"),
+                disabled=get_first("disabled"),
+                location=get_first("location")
+            )
+        )
+
+
 class MetadataConfig(HeuristicConfig):
     def detector(self) -> Type[Detector]:
         module_name, _, class_name = self.location.rpartition(".")
@@ -84,9 +103,23 @@ class ConfigFile:
                 )
             )
 
+    def get_sourcecode(self) -> Iterator[SourcecodeConfig]:
+        for key, item in self._contents.get("sourcecode", {}).items():
+            yield (
+                SourcecodeConfig(
+                    key=key,
+                    name=item.get("name"),
+                    category=item.get("category"),
+                    description=item.get("description"),
+                    disabled=item.get("disabled"),
+                    location=item.get("location")
+                )
+            )
+
 
 class Config:
     _metadata: dict[str, MetadataConfig] = {}
+    _sourcecode: dict[str, SourcecodeConfig] = {}
 
     def add_config(self, path: str):
         config_file = ConfigFile(path)
@@ -95,8 +128,16 @@ class Config:
             existing_metadata = self._metadata.get(metadata.key)
             self._metadata[metadata.key] = metadata.join(existing_metadata)
 
+        for sourcecode in config_file.get_sourcecode():
+            existing_sourcecode = self._sourcecode.get(sourcecode.key)
+            self._sourcecode[sourcecode.key] = sourcecode.join(existing_sourcecode)
+
         return self
 
     @property
     def metadata(self) -> Iterator[MetadataConfig]:
         return iter(self._metadata.values())
+
+    @property
+    def sourcecode(self) -> Iterator[SourcecodeConfig]:
+        return iter(self._sourcecode.values())
